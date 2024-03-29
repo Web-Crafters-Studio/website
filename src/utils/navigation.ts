30dcc41e46e88112e6671f5b7cd2d693a29992cb
Nicolas TOISON <toisonnicolas@gmail.com>
2024-03-29 14:41:51 +0100

    refactor(app): update website to fit with studios content

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,39 +1,33 @@
 // An array of links for navigation bar
 const navBarLinks = [
   { name: "Home", url: "/" },
-  /* { name: "Products", url: "/products" }, */
   { name: "Services", url: "/services" },
-  { name: "Blog", url: "/blog" },
-  /* { name: "Contact", url: "/contact" }, */
 ];
 // An array of links for footer
 const footerLinks = [
   {
-    section: "Ecosystem",
+    section: "Notre entreprise",
     links: [
-      { name: "Documentation", url: "/welcome-to-docs/" },
-      { name: "Tools & Equipment", url: "/products" },
-      { name: "Construction Services", url: "/services" },
+      { name: "Expertises", url: "/services" },
+      { name: "Nous contacter", url: "/contact" },
     ],
   },
   {
-    section: "Company",
+    section: "Legal",
     links: [
-      { name: "About us", url: "#" },
-      { name: "Blog", url: "#" },
-      { name: "Careers", url: "#" },
-      { name: "Customers", url: "#" },
+      { name: "Mentions Légales", url: "/mentions-legales" },
+      { name: "Politique de confidentialité", url: "/politique-confidentialite" },
     ],
   },
 ];
 // An object of links for social icons
 const socialLinks = {
   facebook: "#",
-  x: "#",
-  github: "https://github.com/mearashadowfax/ScrewFast",
+  x: "https://twitter.com/web_crafters35",
+  github: "https://github.com/Web-Crafters-Studio",
   google: "#",
   slack: "#",
-  discord: "#"
+  discord: "#",
 };
 
 export default {
