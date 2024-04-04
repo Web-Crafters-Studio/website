b2ac3346a3e68af2f1d462c76fd4327c02dfcd25
Nicolas TOISON <toisonnicolas@gmail.com>
2024-04-04 11:13:34 +0200

    style(manisfest): update manifest information

--- src/pages/manifest.json.ts
+++ src/pages/manifest.json.ts
@@ -44,8 +44,8 @@ export const GET: APIRoute = async () => {
   );
 
   const manifest = {
-    short_name: "ScrewFast",
-    name: "ScrewFast",
+    short_name: "Web Crafters",
+    name: "Studio Web Crafters",
     icons,
     display: "minimal-ui",
     id: "/",
