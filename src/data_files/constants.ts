26e1210ba6937c41839f5af19aa653dcb0919029
Nicolas TOISON <toisonnicolas@gmail.com>
2024-04-05 12:28:57 +0200

    feat(structured data): update structured data object

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -3,8 +3,10 @@ import ogImageSrc from "@/images/social.png";
 export const SITE = {
   title: "Studio Web Crafters",
   tagline: "Agence web à Saint-Malo",
-  description: "Web Crafters Studio offre un accompagnement premium aux TPE et PME pour les aider à se démarquer sur internet. Contactez notre équipe d'expert pour convenir d'une réunion de lancement.",
-  description_short: "Web Crafters Studio la nouvelle agence web malouine qui booste votre visibilité et vous aide à vous démarquer sur internet.",
+  description:
+    "Web Crafters Studio offre un accompagnement premium aux TPE et PME pour les aider à se démarquer sur internet. Contactez notre équipe d'expert pour convenir d'une réunion de lancement.",
+  description_short:
+    "Web Crafters Studio la nouvelle agence web malouine qui booste votre visibilité et vous aide à vous démarquer sur internet.",
   url: "https://web-crafters.studio",
   author: "Nicolas TOISON",
 };
@@ -13,18 +15,28 @@ export const SEO = {
   title: SITE.title,
   description: SITE.description,
   structuredData: {
-    "@context": "https://schema.org",
-    "@type": "WebPage",
-    inLanguage: "fr-FR",
-    "@id": SITE.url,
-    url: SITE.url,
+    "@context": "http://www.schema.org",
+    "@type": "ProfessionalService",
     name: SITE.title,
-    description: SITE.description_short,
-    isPartOf: {
-      "@type": "WebSite",
-      url: SITE.url,
-      name: `${SITE.tagline} |  ${SITE.title}`,
-      description: SITE.description,
+    url: SITE.url,
+    description: SITE.description,
+    address: {
+      "@type": "PostalAddress",
+      streetAddress: "10 Avenue Anita Conti",
+      addressLocality: "Saint-Malo",
+      addressRegion: "Bretagne",
+      postalCode: "35400",
+      addressCountry: "FR",
+    },
+    geo: {
+      "@type": "GeoCoordinates",
+      latitude: "48.64744787492076",
+      longitude: "-2.0046975669908194",
+    },
+    openingHours: "Mo, Tu, We, Th, Fr 09:30-18:00",
+    contactPoint: {
+      "@type": "ContactPoint",
+      telephone: "+33223172236",
     },
   },
 };
